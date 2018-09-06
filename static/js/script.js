c20d2574178407a071dcfc58b518938a3ddbc62e
Yakovlev Gleb <brin@ya.ru>
2018-09-06 09:46:18 +0500

    adaptive

--- static/js/script.js
+++ static/js/script.js
@@ -15,6 +15,20 @@ $(document).ready(function() {
 				showMaskOnFocus: false
 			});
 
+			$('.js-input-mask').on('keydown', function(e) {
+				var validArr = [46, 8, 9, 27, 13, 110, 190]
+				if (validArr.indexOf(e.keyCode) !== -1 ||
+				(e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+				(e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+				(e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+				(e.keyCode >= 35 && e.keyCode <= 39)) {
+					return;
+				}
+				if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+					e.preventDefault()
+				}
+			});
+
 			$('.js-input-mask').on('keyup', function() {
 				var _t = $(this);
 				var val = _t.val().replace(/\s/g, '');
